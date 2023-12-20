03d2270f896e27a3788ab57d2fdc2cbfe2e7eac8
unknown <leopeter800@gmail.com>
2023-12-20 11:12:26 +0800

    Regularly Updated

---
+++ autogit/AutoGitUpdate.bat
@@ -0,0 +1,4 @@
+cd /d "C:\Users\w\spyder_workspace_W"
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m "Regularly Updated"
+"C:\Program Files\Git\bin\git.exe" push
