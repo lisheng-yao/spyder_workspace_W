a8b36af3613ee0a3c5c6582d40038f394426ac86
unknown <leopeter800@gmail.com>
2023-12-20 12:30:00 +0800

    Regularly Updated

--- testPandas.py
+++ testPandas.py
@@ -63,3 +63,34 @@ dictionary = {'A':'an animal',
 dictionary = pd.Series(dictionary)
 dictionary
 # dtype: object
+
+#%% 合併csv
+
+df1 = pd.read_csv(r"C:\Users\w\Desktop\data\bank1.csv")
+df2 = pd.read_csv(r"C:\Users\w\Desktop\data\bank2.csv")
+
+combined_df = pd.concat([df1, df2], axis=0)
+# 使用concat函数合并数据表，axis=0表示在垂直方向上合并（往下）
+combined_df.reset_index(drop=True, inplace=True)
+
+# 重置索引，因为concat后索引会保持原数据表的索引
+combined_df.reset_index(drop=True, inplace=True)
+
+# 如果您想要将合并后的数据表保存为新的CSV文件
+try:
+    combined_df.to_csv(r"C:\Users\w\Desktop\merged_data.csv", index=False)
+    print("合併成功")
+except Exception as e:
+    print("合併失敗:", e)
+
+
+# from glob import glob
+# files = glob(r"C:\Users\w\Desktop\data\bank*.csv")
+
+# df = pd.concat([pd.read_csv(f) for f in files])
+# df.reset_index(drop = True)
+
+# 輸出合併CSV檔
+# output_file_path = r"C:\Users\w\Desktop\merged_data.csv"  # 指定輸出文件的路徑
+# df.to_csv(output_file_path, index=False)  # index=False 表示不將行索引寫入到 CSV 文件中
+
