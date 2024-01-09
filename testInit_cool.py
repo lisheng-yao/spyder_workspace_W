ceb92cb98bf8cf12599e641e8b07fb66b3d36fc5
unknown <leopeter800@gmail.com>
2024-01-09 13:44:26 +0800

    Regularly Updated

---
+++ testInit_cool.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 13:36:12 2024
+
+@author: w
+"""
+
+# cool.py
+def my_cool_function():
+    print("這超酷!")
+    # print("__name__:", __name__) # 顯示內建的 __name__ 值
+
+if __name__ == "__main__" :
+    print("呼叫 my_cool_function")
+    my_cool_function()
