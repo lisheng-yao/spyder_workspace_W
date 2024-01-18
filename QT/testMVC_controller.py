b73a3ed7474247ac729ab48c1b46da72febce2e7
unknown <leopeter800@gmail.com>
2024-01-18 17:30:00 +0800

    Regularly Updated

--- QT/testMVC_controller.py
+++ QT/testMVC_controller.py
@@ -25,7 +25,7 @@ class TestMVC_controller(QMainWindow):
         self.set_logo()
         self.click_count = 0
         
-
+    #    
     def button_click(self):
         self.click_count += 1
         self.view.pushButton.setText(str(self.click_count))
@@ -38,7 +38,7 @@ class TestMVC_controller(QMainWindow):
         print( "x: " + str(random_a)  +   " y : " +str(random_b) )
         
         
-        
+    #    
     def upload_click(self):
         options = QFileDialog.Options() # 配置文件對話框物件
         file_path, _ = QFileDialog.getOpenFileName(None, "未上傳檔案", "", "CSV Files (*.csv);;All Files (*)", options=options)
@@ -48,7 +48,7 @@ class TestMVC_controller(QMainWindow):
             self.view.label.setText(f"已選擇檔案：{file_path}")
             self.data_preview(file_path)
     
-    
+    #
     def data_preview(self,file_path):
         data = pd.read_csv(file_path)
         # print(data)
@@ -65,30 +65,21 @@ class TestMVC_controller(QMainWindow):
     # 
     def on_combobox_changed(self, index):
         self.view.stackedWidget.setCurrentIndex(index)
+    
         
-
+    #    
     def set_logo(self):
         print('logo已設定')
         scene = QtWidgets.QGraphicsScene()
         img = QtGui.QPixmap(r'C:\Users\w\spyder_workspace_W\QT\logo\logo_normal.png')
         img = img.scaled(400,80)
         scene.addPixmap(img)
-        self.view.graphicsView.setScene(scene)
+        self.view.graphicsView_logo.setScene(scene)
 
 
-# 1.你有反應是1月底的沒辦法做完(某個東西)，DDL阻塞問題5分鐘能解決，卻重構了多次，gpt使用缺點
-# 2.我目前工作應著重在軟體工程師學習，並協助你完成任務，而不是只有ai
-# 3.你有提到台塑對不起我或你之類?但應該先做出東西再來談
-# 4.跟你討論的工作、學習方向，須更考慮到能為團隊或主管做什麼
-# 5.規劃的ai功能不應該是我們這樣決定的，應該要以市場需求為出發點
-# 6.要更理解產業內容才可以找到市場與需求
 
 
-# 寒號鳥相反，積極主動、負責任、勤奮努力
-# 我一直以來都有反應，希望能為團隊帶來助111益並從中學習到技能
-# 會選擇在台北一個月領不到3萬多，寧願生活品質，想得到的絕對不只是福利
-# 1.討論需要建立可視化紀錄，讓主管更新掌握
-# 2.
+
 
 
 # 在跟W討論時，沒有著重在這些
