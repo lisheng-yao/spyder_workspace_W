12ef93ad08b7b49e86bf2133d558f4b72a605441
lisheng-yao <>
2024-01-25 00:32:08 +0800

    240125 form mac

--- windowsAutoGit/AutoGitUpdate.bat
+++ windowsAutoGit/AutoGitUpdate.bat
@@ -1,4 +1,5 @@
 cd /d "C:\Users\w\spyder_workspace_W"
+"C:\Program Files\Git\bin\git.exe" pull
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -m "Regularly Updated"
 "C:\Program Files\Git\bin\git.exe" push
