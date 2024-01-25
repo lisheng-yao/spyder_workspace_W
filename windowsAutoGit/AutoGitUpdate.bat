3b1dd90237a03929d847d8a1cd8cc1e6c9603142
unknown <leopeter800@gmail.com>
2024-01-25 09:11:01 +0800

    Regularly Updated

--- windowsAutoGit/AutoGitUpdate.bat
+++ windowsAutoGit/AutoGitUpdate.bat
@@ -1,11 +1,4 @@
 cd /d "C:\Users\w\spyder_workspace_W"
-"C:\Program Files\Git\bin\git.exe" fetch
-"C:\Program Files\Git\bin\git.exe" diff HEAD origin/master --exit-code > nul 2>&1
-if errorlevel 1 (
-    echo 有未合併的更改，請先處理。
-    pause
-    exit /b
-)
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -m "Regularly Updated"
 "C:\Program Files\Git\bin\git.exe" push
