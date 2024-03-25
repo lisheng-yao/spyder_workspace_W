df92092048c9c1a79dc5d453fbdf41420249b051
Leo <leopeter800@gmail.com>
2024-03-25 15:30:00 +0800

    Regularly Updated

--- untitled1.py
+++ untitled1.py
@@ -1,30 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 17 12:02:51 2024
+Created on Mon Mar 25 15:21:38 2024
 
 @author: w
 """
 
-import pandas as pd 
-
-dic = {
-    "col 1": [1, 2, 3], 
-    "col 2": [10, 20, 30],
-    "col 3": list('xyz'),
-    "col 4": ['a', 'b', 'c'],
-    "col 5": pd.Series(range(3))
-}
+import random
 
 
-df = pd.DataFrame(dic)
-
-print(df)
-
-print(f'df的型別是: {type(df)}')
-print(f'dic的型別是: {type(dic)}')
+a = set()
 
 
 
-
-# {}回傳是什麼東西
-# df是什麼東西
+for j  in range(6):
+    for i in range (1,49):
+        a.add(i)
+        
+print(a)
